4a0d612be67303fb20808eadfa64c433c9010533
NRSCARLET <107818695+NRSCARLET@users.noreply.github.com>
2023-08-07 23:27:08 +0000

    Copied and pasted from my broken code/github repo. Changed the text so I can have a more accurate base for my game.

--- BASETKINTER.py
+++ BASETKINTER.py
@@ -1,12 +1,12 @@
 import tkinter as tk
 
 window = tk.Tk()
-window.title("Hello world")
+window.title("Spelling Bee Spelling Game")
 window.geometry("300x300")
 
-hello = tk.Label(text="Hello world!")
+hello = tk.Label(text="Welcome to the Spelling Bee Spelling Game!")
 hello.pack()
-button = tk.Button(text="Click me!")
+button = tk.Button(text="Test Button")
 button.pack()
-
+ 
 tk.mainloop()
