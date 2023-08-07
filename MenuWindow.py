c0d8f16bd0dfaf5ab71160e0cdbb5de3d9cb7809
NRSCARLET <107818695+NRSCARLET@users.noreply.github.com>
2023-08-07 23:51:51 +0000

    Destroyed the buttons and text from MenuWindow.py and inserted text onto same window. Need to add a text box for user to insert username.

---
+++ MenuWindow.py
@@ -0,0 +1,22 @@
+from tkinter import *
+import tkinter as tk
+
+window = tk.Tk()
+window.configure (bg = '#6693F5')
+window.title("Spelling Bee's Spelling Game!")
+window.geometry("400x400")
+
+def login():
+    b1.destroy()
+    b2.destroy()
+    hello.destroy()
+    import UserLogin
+    UserLogin
+    
+hello = tk.Label(text="Welcome to Spelling Bee's Spelling Game!", fg = 'white', bg = '#6693F5', font = 'helvetica 12 bold')
+hello.pack()
+b1 = tk.Button(text="Register (new user)", fg = 'white', bg = '#DA70D6', font = 'helvetica 9 bold')
+b1.pack()
+b2 = tk.Button(text="Login (old user)", fg = 'white', bg = '#DA70D6', font = 'helvetica 9 bold', command=login)
+b2.pack()
+tk.mainloop()
---
+++ UserLogin.py
@@ -0,0 +1,12 @@
+import sys 
+from tkinter import *
+import tkinter as tk 
+#PrintLogger code from Quora
+
+login = tk.Label(text="Please input a Username!")
+login.pack()
+
+ 
+tk.mainloop()
+
+    
