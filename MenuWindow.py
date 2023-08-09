3ecd94e895e6e5aa910c9f45756a783b4bc5b386
NRSCARLET <107818695+NRSCARLET@users.noreply.github.com>
2023-08-09 01:58:18 +0000

    Try to start making the user register screen + the back button on the Login Screen + exit button on Menu Screen

--- MenuWindow.py
+++ MenuWindow.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import tkinter as tk
-
+from os import system, name
+from time import sleep
 window = tk.Tk()
 window.configure (bg = '#6693F5')
 window.title("Spelling Bee's Spelling Game!")
@@ -11,7 +12,13 @@ def clear():
         _ = system('cls')
     else:
         _ = system('clear')
-
+def register():
+    b1.destroy()
+    b2.destroy()
+    hello.destroy()
+    window.destroy()
+    import UserReg
+    UserReg
 def login():
     b1.destroy()
     b2.destroy()
@@ -19,13 +26,20 @@ def login():
     window.destroy()
     import UserLogin
     UserLogin
+    
 def exit():
-    quit()
+    b1.destroy()
+    b2.destroy()
+    b3.destroy()
+    hello.destroy()
+    goodbye = tk.Label(text="Thank you for playing!").pack()
+    
 hello = tk.Label(text="Welcome to Spelling Bee's Spelling Game!", fg = 'white', bg = '#6693F5', font = 'helvetica 12 bold')
 hello.pack()
-b1 = tk.Button(text="Register (new user)", fg = 'white', bg = '#DA70D6', font = 'helvetica 9 bold')
+b1 = tk.Button(text="Register (new user)", fg = 'white', bg = '#DA70D6', font = 'helvetica 9 bold', command=register)
 b1.pack()
 b2 = tk.Button(text="Login (old user)", fg = 'white', bg = '#DA70D6', font = 'helvetica 9 bold', command=login)
-b3 = tk.Button(text="Exit Game", fg = 'white', bg = '#DA70D6', font = 'helvetica 9 bold', command=exit)
 b2.pack()
+b3 = tk.Button(text="Exit Game", fg = 'white', bg = '#DA70D6', font = 'helvetica 9 bold', command=exit)
+b3.pack()
 tk.mainloop()
