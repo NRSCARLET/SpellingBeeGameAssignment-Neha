50496f11a53852e49cd539f7ab9909aa65c7dfd0
NRSCARLET <107818695+NRSCARLET@users.noreply.github.com>
2023-08-13 23:34:30 +0000

    starting to make the easy mode of my spelling game.

--- Difficulty.py
+++ Difficulty.py
@@ -3,18 +3,31 @@ from tkinter import *
 import tkinter as tk
 from tkinter import simpledialog
 
+def Easymode():
+    diff.destroy()
+    import easyspell
+    easyspell
+
+def Medmode():
+    diff.destroy()
+    import medspell
+    medspell
+
+def Hardmode():
+    diff.destroy()
+    import hardspell
+    hardspell
 
 diff = Tk()
 diff.geometry("200x130")
 diff.configure (bg = '#6693F5')
 diff.title("Spelling Bee's Spelling Game!")
-
 dif = tk.Label(text="Choose a difficulty")
 dif.pack()
 
-easybut = tk.Button(text="Easy")
+easybut = tk.Button(text="Easy", command = Easymode)
 easybut.pack()
-medbut = tk.Button(text="Medium")
+medbut = tk.Button(text="Medium", command = Medmode)
 medbut.pack()
-hardbut = tk.Button(text="Hard")
+hardbut = tk.Button(text="Hard", command = Hardmode)
 hardbut.pack()
---
+++ easyspell.py
@@ -0,0 +1,5 @@
+import sys 
+from tkinter import *
+import tkinter as tk
+from tkinter import simpledialog
+
