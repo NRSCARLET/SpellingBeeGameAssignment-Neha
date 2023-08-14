440cad47bde43b4ef7f9062a3c51ee4ae07f36c8
NRSCARLET <107818695+NRSCARLET@users.noreply.github.com>
2023-08-14 23:37:34 +0000

    added base to medium and hard difficulty

---
+++ Hardspell.py
@@ -0,0 +1,11 @@
+import sys 
+from tkinter import *
+import tkinter as tk
+from tkinter import simpledialog
+
+har = Tk()
+har.geometry('200x200')
+har.configure(bg = '#6693F5')
+har.title("Spelling Bee's Spelling Game!")
+h = tk.Label(text="wow hard")
+h.pack()
---
+++ Medspell.py
@@ -0,0 +1,11 @@
+import sys 
+from tkinter import *
+import tkinter as tk
+from tkinter import simpledialog
+
+med = Tk()
+med.geometry('200x200')
+med.configure(bg = '#6693F5')
+med.title("Spelling Bee's Spelling Game!")
+m = tk.Label(text="wow medium")
+m.pack()
