3abe7dfa8b6bd7c35fed38c76d0bf08436a41b24
NRSCARLET <107818695+NRSCARLET@users.noreply.github.com>
2023-08-17 02:58:48 +0000

    trying to append new files onto text file. won't work. Found a YouTube video showing what I could do.. Will follow it later.

--- ReadingTextFiles.py
+++ ReadingTextFiles.py
@@ -1,4 +1,32 @@
-names = open('names.txt', 'r')
+"""names = open('names.txt', 'r')
 names = names.read()
 name_list = names.split()
 print(name_list)
+
+names = open('names.txt', 'a')
+
+print("what your name")
+inputname = input()
+
+names.write(inputname)
+print(names)"""
+
+#Take input from user and assign it to variables
+"""print("What is your first name?")
+input1 = input()
+print("What is your last name?")
+input2 = input()
+ 
+#Open the text.txt file for appending.
+ 
+file1 = open('text.txt', 'a')
+ 
+#Write the content of the variables to the text.txt file
+file1.write(input1)
+file1.write(" ")
+file1.write(input2)
+file1.write("\n")
+ 
+#Close the text.txt file
+file1.close()"""
+
