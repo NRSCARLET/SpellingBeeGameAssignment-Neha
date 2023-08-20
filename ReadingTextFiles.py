7854b6109cb971fad83b7b3185bb70c596fd8e16
NRSCARLET <107818695+NRSCARLET@users.noreply.github.com>
2023-08-20 22:48:01 +0000

    Got ChatGPT to help out with reading from the text file. Finally got it to work. Will rewrite it on my own to suit my login/register menu for my spelling game.

--- ReadingTextFiles.py
+++ ReadingTextFiles.py
@@ -30,8 +30,14 @@ file1.write("\n")
 #Close the text.txt file
 file1.close()"""
 
-#Trying code from YouTube video
+"""print("Please create a username\n>> ")
+user = input()
+file = open('username.txt', 'a')
+file.write(user)"""
 
+
+
+#Trying code from YouTube video
 """def reg():
     db = open('names.txt', 'r')
     username = input("Please create a username")
@@ -47,5 +53,98 @@ file1.close()"""
         print(data)
 
 reg()"""
+
+#stackoverflow
+"""def Register():
+    print("Hello! You need to register an account before you can begin")
+    username = input("Please enter a username: ")
+    password = input("Now please enter a password: ")
+    file = open("Login.txt","a")
+    file.write (username)
+    file.write (",")
+    file.write (password)
+    file.write("\n")
+    file.close()
+    print ("Your login details have been saved. ")
+    print("You will now need to login")
+    Login() 
+
+def Login():
+    print("Please enter your details to log in")
+    username1 = input("Please enter your username: ")
+    password1 = input("Please enter your password: ")
+
+    file = open("Login.txt","r")
+
+    for row in file:
+        field = row.split(",")
+        username = field[0]
+        password = field[1]
+        lastchar = len(password)-1
+        password = password[0:lastchar]
+
+    if username1 == username and password1 == password:
+        print("Hello",username)
+
+    else:
+
+#file.close()
+
+
+user=input("Are you already a user? ")
+
+if user == "Yes":
+         Login()
     
+elif user =="No":
+         Register()
     
+print("Welcome to our game")"""
+
+#ChatGPT help
+def reg():
+    username = input("Please create a username: ")
+    with open('username.txt', 'a') as file:
+        file.write(username + "\n")
+    print("Your username has been saved")
+    login()
+
+def login():
+    with open('username.txt', 'r') as file:
+        usernames = [line.strip() for line in file.readlines()]
+    print("Usernames in the file:", usernames)
+    userlogin = input("Enter your username: ")
+    userlogin = userlogin.strip()
+    if userlogin in usernames:
+        print("Welcome")
+    else:
+        print("This username does not exist")
+
+
+def question():
+    ask = input("reg or login: ")
+    if ask == 'reg':
+        reg()
+    elif ask == 'login':
+        login()
+    else:
+        print("Invalid choice")
+        question()
+
+question()
+
+"""Changes made:
+
+Used the with statement to properly open and close files, ensuring proper resource management.
+Read the lines from the file and stored them in the usernames list for comparison.
+Removed leading and trailing whitespace from the entered username using the strip() method.
+Changed the comparison to check if the entered username is in the list of usernames.
+Now the code should work as expected, allowing users to register and log in with their usernames."""
+
+
+
+
+
+
+    
+
