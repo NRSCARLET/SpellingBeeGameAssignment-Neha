4b7f708bc73e8f9cc563ea4e972b46f0de99b433
NRSCARLET <107818695+NRSCARLET@users.noreply.github.com>
2023-09-10 21:34:14 +0000

    Deleted file because its easier to quickly play through game rather than have to tweak code to work on its own. Entry box not working either

--- Easyspell.py
+++ Easyspell.py
@@ -65,7 +65,6 @@ def actualgame():
     global AnswerEntry, printed_key, correct_answer, level, enterbutton, wordbutton
     level +=1
     answerlabel.config(text="")
-    MAX_CHRCTRS = 3
     if level <= 10:
         printed_key = random.choice(list(easy_spell_words_dict))
         correct_answer = easy_spell_words_dict.pop(printed_key)
@@ -77,17 +76,8 @@ def actualgame():
         AnswerEntry.grid(row=2, column=0, padx=5, pady=5)
         AnswerEntry.delete(0, tk.END)
     else:
-        wordbutton.config(text="End Game!", state = "disabled")
+        wordbutton.config(text="End Game!")
         points = userpoints
-        leaderboard = Label(text="Please enter a 3 character name for the leaderboard!")
-        leaderboard.grid(row=4, column=0, padx=3, pady=3)
-        leaderboardEntry = tk.Entry(ez, bd=5)
-        leaderboardEntry.grid(row=5, column=0, padx=5, pady=5)
-        LBENT = leaderboardEntry.get()
-        if len(LBENT) == 3:
-            wordbutton.config(state = "active")
-        else:
-            leaderboardEntry.delete(MAX_CHRCTRS, tk.END)
         with open('easyscore.txt', 'a') as pointopen:
             pointopen.write(str(points) + "\n")
         ez.destroy()
